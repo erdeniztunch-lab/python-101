9744db4066350c51a190c6770d90421b3d05c67a
Erdeniz Tunc <erdeniztunch@gmail.com>
2026-03-07 16:58:06 +0300

    day 1 completed

---
+++ day1/11-day1-summary.py
@@ -0,0 +1,133 @@
+# Quick Summary - Day 1
+
+# 1- What is Python
+# Python is a simple and readable programming language for web development, automation, data analysis, and AI.
+
+# ----------------------------------------
+
+# 2- How Python Programs Work
+# Python code usually runs line by line.
+# Each line is usually a statement (instruction)
+
+# Example
+print("Hello")
+print("World")
+
+# Python runs the first line, then the second.
+
+# ----------------------------------------
+
+# 3- Python Syntax (basic rules)
+# Two important rules in Python:
+
+# 1- New lines
+# Each instruction is usually written on a new line.
+
+# 2- Indentation
+# Indentation (spaces at the beginning of a line) tells Python which code belongs together.
+
+# Example
+
+for i in range(3):
+    print(i)
+
+# The print line is inside the loop because it is indented.
+
+# ----------------------------------------
+
+# 4- Statements
+# A statement is a single instruction given to the computer.
+
+print("Python is fun")
+
+# A program is usually many statements executed in order.
+
+# ----------------------------------------
+
+# 5- Output (print)
+# The print() function is used to display output on the screen.
+
+print("Hello World")
+
+# Python can print both text and numbers.
+
+print(5)
+print(2+4)
+
+# ----------------------------------------
+
+# 6- Comments
+# Comments are notes in the code that Python ignores.
+
+# They start with -> #
+
+# Example
+
+# THIS IS A COMMENT!
+
+# Comments help:
+# Explain code
+# Temporarily disable code
+
+# ----------------------------------------
+
+# 7- Variables
+# A variable is a container used to store data.
+
+# Example
+q = 5
+name = "John"
+
+# Variables can store:
+# numbers
+# text
+# other data types
+
+# ----------------------------------------
+
+# Variables name rules
+
+# A variable name:
+
+# must start with a letter or _
+# cannot start with a number
+# cannot contain spaces
+# is case-sensitive
+
+# Example
+age = 25
+_name = "John"
+
+# ----------------------------------------
+
+# 5 Questions
+
+# 1- Which function is used to print text in Python?
+# 2- Which symbol is used to create a comment in Python?
+# 3- In Pythın, each line of code usually represents what?
+# 4- Do you need a special keyword to create a variable in Python?
+# 5- What will this code print?
+
+p = "Python"
+print(p)
+
+# ----------------------------------------
+# ----------------------------------------
+# ----------------------------------------
+# ----------------------------------------
+# ----------------------------------------
+
+# Answers:
+
+# 1- print()
+
+# 2- #
+
+# 3- A statement (an istruction for the computer)
+
+# 4- No. A variable is created when you assign a value
+
+# Example:
+e = 1
+
+# 5- Output: Python
